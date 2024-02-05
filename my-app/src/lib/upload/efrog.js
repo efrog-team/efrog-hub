4f8e84fdf1ab72d57366bc2c642e96b4632de0cd
RagnarLodbrock912 <137827970+RagnarLodbrock912@users.noreply.github.com>
2024-02-05 22:28:51 +0200

    secure boot efrog

---
+++ my-app/src/lib/upload/efrog.js
@@ -0,0 +1,157 @@
+const fs = require('fs');
+const fse = require('fs-extra');
+const AdmZip = require('adm-zip');
+const stream = require('stream');
+
+// const base64String = process.argv[2];
+// const curent_sesion = process.argv[3];
+// const main_dir = process.argv[4];
+// const fileName = process.argv[5];
+
+// if(!base64String){
+//     console.error('base64')
+// }
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Чтение данных из stdin
+rl.on('line', (input) => {
+    // Ваш код обработки данных
+    const data = JSON.parse(input)
+
+    const res = processFile(data.base64String, data.curent_sesion, data.currentDirectory, data.fileName);
+    if(typeof res == 'string'){
+        console.log(res);
+    }
+    else{
+        console.error(res.error);
+    }
+
+    // Закрытие интерфейса чтения данных из stdin
+    rl.close();
+});
+
+function checkValue(value, min, max) {
+    const regex = /^\d+$/;
+
+    if (!regex.test(value)) {
+    return false; 
+    }
+
+    const numericValue = parseFloat(value);
+
+    if (!isNaN(numericValue) && numericValue >= min && numericValue <= max) {
+        return true; 
+    }
+
+    return false; 
+}
+
+function processFile(base64String, curent_sesion, main_dir, fileName) {
+    try{
+        let name, time_limit, memory_limit, statement, input_statement, output_statement, note;
+        let test = [];
+
+        if (fs.existsSync(`./files/upload/efrog_${curent_sesion}`)) {
+            fse.removeSync(`./files/upload/efrog_${curent_sesion}`);
+        }
+        fs.mkdirSync(`./files/upload/efrog_${curent_sesion}`);
+
+        let targetFilePath = `${main_dir}/files/upload/efrog_${curent_sesion}/${fileName}`;
+
+        // await createFileFromBase64(base64String, targetFilePath);
+        const fileBuffer = Buffer.from(base64String, 'base64');
+        fs.writeFileSync(targetFilePath, fileBuffer);
+        
+        let zip = new AdmZip(targetFilePath);
+        zip.extractAllTo(`${main_dir}/files/upload/efrog_${curent_sesion}/`);
+        fs.unlinkSync(targetFilePath);
+        
+        let main_path = `${main_dir}/files/upload/efrog_${curent_sesion}/`;
+        
+        //Check folder with task about nesessary folders and files
+        const directories = [
+            // general_info
+            'general_info', 'general_info/name.txt', 'general_info/time_limit.txt', 'general_info/memory_limit.txt',
+            // statement
+            'statement', 'statement/statement.tex',
+            // examples
+            'examples', 'examples/input', 'examples/output',
+            // tests
+            'tests', 'tests/input', 'tests/output'
+        ];
+        
+        for (let i = 0; i < directories.length; i++) {
+            if (!fs.existsSync(main_path + directories[i])) {
+                fse.removeSync(main_path);
+                return(JSON.stringify({status: 404, error: `There are not ${directories[i]}`}));
+            }
+        }
+        
+        // Process general info
+        name = fs.readFileSync(main_path + 'general_info/name.txt', 'utf-8');
+        time_limit = fs.readFileSync(main_path + 'general_info/time_limit.txt' , 'utf-8');
+        memory_limit = fs.readFileSync(main_path + 'general_info/memory_limit.txt', 'utf-8');
+            
+        if(!checkValue(time_limit, 1, 10)){
+            return(JSON.stringify({status: 404, error: `Time must be between 1 and 10 seconds`}));
+        }
+        if(!checkValue(memory_limit, 4, 1024)){
+            return(JSON.stringify({status: 406, error: `Memory must be between 4 and 1024 MB`}));
+        }
+        
+        // Process statement
+        statement = fs.readFileSync(main_path + 'statement/statement.tex', 'utf-8');
+        
+        if(fs.existsSync(main_path + 'statement/input_statement.tex')){
+            input_statement = fs.readFileSync(main_path + 'statement/input_statement.tex', 'utf-8');
+        }
+            
+        if(fs.existsSync(main_path + 'statement/output_statement.tex')){
+            output_statement = fs.readFileSync(main_path + 'statement/output_statement.tex', 'utf-8');
+        }
+            
+        if(fs.existsSync(main_path + 'statement/note.tex')){
+            note = fs.readFileSync(main_path + 'statement/note.tex', 'utf-8');
+        }
+        
+        // Process test cases
+        test = [];
+        const testCases = [
+            { inputPath: 'examples/input/', outputPath: 'examples/output/', status: "Opened" },
+            { inputPath: 'tests/input/', outputPath: 'tests/output/', status: "Closed" }
+        ];
+                
+        for (const testCase of testCases) {
+            const inputPath = main_path + testCase.inputPath;
+            const outputPath = main_path + testCase.outputPath;
+            const inputFiles = fs.readdirSync(inputPath);
+        
+            for (const currentFile of inputFiles) {
+                const inputFilePath = inputPath + currentFile;
+                const outputFilePath = outputPath + currentFile;
+        
+                if (fs.existsSync(outputFilePath)) {
+                    const input = fs.readFileSync(inputFilePath, 'utf-8');
+                    const output = fs.readFileSync(outputFilePath, 'utf-8');
+                    const id = test.length + 1;
+                    test.push({ test_id: id, input, output, status: testCase.status });
+                } else {
+                    fse.removeSync(main_path);
+                    return(JSON.stringify({status: 404, error: `Test${currentFile} exists in ${testCase.inputPath}, but does not exist in ${testCase.outputPath}`}));
+                }
+            }
+        }
+        return(JSON.stringify({name, time_limit, memory_limit, statement, input_statement, output_statement, note, test}));
+    }
+    catch(e){
+        console.error(e)
+        return({error: e})
+    }
+    
+}
--- my-app/src/routes/api/task/finish/upload-file/+server.js
+++ my-app/src/routes/api/task/finish/upload-file/+server.js
@@ -1,12 +1,9 @@
 import { json } from '@sveltejs/kit';
-import AdmZip from 'adm-zip';
-import fs from 'fs';
 import fse from 'fs-extra';
 import { getClientrId } from '$lib/server/main';
-import { authorization, checkValue } from '$lib/server/check.js';
-
-let name, time_limit, memory_limit, statement, input_statement, output_statement, note;
-let test = [];
+import { authorization} from '$lib/server/check.js';
+import { spawnSync } from 'child_process';
+import path from 'path';
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST( {request, cookies} ) {
@@ -22,108 +19,79 @@ export async function POST( {request, cookies} ) {
         if (!verdict) {
             return json({ error: `Tou are not author of that task` }, { status: 403 });
         }
-        // Get task arhive and save them into folder
+        // Get task arhive and make base64 to give it into child process
         const file = formData.get('file');
+        const fileName = file.name;
 
-        if (fs.existsSync(`./files/upload/efrog_${curent_sesion}`)) {
-            fse.removeSync(`./files/upload/efrog_${curent_sesion}`);
+        const reader = file.stream().getReader();
+        let chunks = [];
+
+        while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        chunks.push(value);
         }
-        fs.mkdirSync(`./files/upload/efrog_${curent_sesion}`);
 
-        let targetFilePath = `./files/upload/efrog_${curent_sesion}/${file.name}`;
-        const fileBuffer = await file.arrayBuffer();
-        fs.writeFileSync(targetFilePath, new Uint8Array(fileBuffer));
+        const fileBuffer = Buffer.concat(chunks);
+        const base64String = fileBuffer.toString('base64');
 
-        let zip = new AdmZip(targetFilePath);
-        zip.extractAllTo("./files/upload/efrog_" + curent_sesion + "/");
-        fs.unlinkSync(targetFilePath);
+        const command = 'node';
+        // Get patht to process file
+        const targetFileName = 'src/lib/upload/efrog.js';
+        const currentDirectory = path.resolve();
+        const scriptPath = path.resolve(currentDirectory, targetFileName);
 
-        let main_path = `./files/upload/efrog_${curent_sesion}/`;
-
-        //Check folder with task about nesessary folders and files
-        const directories = [
-            // general_info
-            'general_info', 'general_info/name.txt', 'general_info/time_limit.txt', 'general_info/memory_limit.txt',
-            // statement
-            'statement', 'statement/statement.tex',
-            // examples
-            'examples', 'examples/input', 'examples/output',
-            // tests
-            'tests', 'tests/input', 'tests/output'
-        ];
-
-        for(let i = 0; i < directories.length; i++) {
-            if (!fs.existsSync(main_path + directories[i])) {
-                fse.removeSync(main_path);
-                return json({ error: `There are not ${directories[i]}` }, { status: 404 });  
+        const inputData = JSON.stringify({
+            base64String,
+            curent_sesion,
+            currentDirectory,
+            fileName
+        });
+        
+        const options = {
+            input: inputData, 
+            stdio: 'pipe',
+            uid: 65534
+        };
+        
+        const result = spawnSync(command, [scriptPath], options);
+        // console.log(result.stdout.toString());
+        // console.log(result.stderr.toString());
+        if (result.status === 0) {
+            console.log(result.stdout.toString())
+            const data = JSON.parse(result.stdout.toString());
+            console.log(data)
+            if(data.error){
+                return json({ error: data.error }, { status: data.status });
             }
-        }
-
-        // Process general info
-        name = fs.readFileSync(main_path + 'general_info/name.txt', 'utf-8');
-        time_limit = fs.readFileSync(main_path + 'general_info/time_limit.txt' , 'utf-8');
-        memory_limit = fs.readFileSync(main_path + 'general_info/memory_limit.txt', 'utf-8');
-    
-        if(!checkValue(time_limit, 1, 10)){
-            return json({ error: `Time must be between 1 and 10 seconds` }, { status: 406 });  
-        }
-        if(!checkValue(memory_limit, 4, 1024)){
-            return json({ error: `Memory must be between 4 and 1024 MB` }, { status: 406 });
-        }
-
-        // Process statement
-        statement = fs.readFileSync(main_path + 'statement/statement.tex', 'utf-8');
-
-        if(fs.existsSync(main_path + 'statement/input_statement.tex')){
-            input_statement = fs.readFileSync(main_path + 'statement/input_statement.tex', 'utf-8');
-        }
-    
-        if(fs.existsSync(main_path + 'statement/output_statement.tex')){
-            output_statement = fs.readFileSync(main_path + 'statement/output_statement.tex', 'utf-8');
-        }
-    
-        if(fs.existsSync(main_path + 'statement/note.tex')){
-            note = fs.readFileSync(main_path + 'statement/note.tex', 'utf-8');
-        }
-
-        // Process test cases
-        test = [];
-        const testCases = [
-            { inputPath: 'examples/input/', outputPath: 'examples/output/', status: "Opened" },
-            { inputPath: 'tests/input/', outputPath: 'tests/output/', status: "Closed" }
-        ];
-        
-        for (const testCase of testCases) {
-            const inputPath = main_path + testCase.inputPath;
-            const outputPath = main_path + testCase.outputPath;
-            const inputFiles = fs.readdirSync(inputPath);
-        
-            for (const currentFile of inputFiles) {
-                const inputFilePath = inputPath + currentFile;
-                const outputFilePath = outputPath + currentFile;
-        
-                if (fs.existsSync(outputFilePath)) {
-                    const input = fs.readFileSync(inputFilePath, 'utf-8');
-                    const output = fs.readFileSync(outputFilePath, 'utf-8');
-                    const id = test.length + 1;
-                    test.push({ test_id: id, input, output, status: testCase.status });
-                } else {
-                    fse.removeSync(main_path);
-                    return json({ error: `Test${currentFile} exists in ${testCase.inputPath}, but does not exist in ${testCase.outputPath}` }, { status: 404 });  
-                }
+            else{
+                return(json(data));
             }
-        }
-
-        // Return task data
-        return json({name, time_limit, memory_limit, statement, input_statement, output_statement, note, test});
+        } else{
+            console.log(result.stderr);
+            if (result.stderr) {
+                const stderrString = result.stderr.toString();
+                console.log(stderrString);
+                return json({ error: stderrString }, { status: 404 });
+            } else {
+                return json({ error: "Unexpected error occurred" }, { status: 500 });
+            }
+            
+        };
     } catch (error) {
         // In instance of error return error
         console.log(error);
-        return json({ error: `There are problems with file uploading. Please try one more time` }, { status: 500 });  
+        return json({ error: `There are problems with file uploading. Please try one more time` }, { status: 500 });
     } finally {
         // Delete folder with the problem in any way
         const curent_sesion = cookies.get('token');
-        let main_path = `./files/upload/efrog_${curent_sesion}/`;
-        fse.removeSync(main_path);
+        const main_path = `./files/upload/efrog_${curent_sesion}/`;
+
+        try {
+            await fse.remove(main_path);
+        } catch (error) {
+            console.error(`Error removing folder: ${main_path}`, error);
+        }
     }
 }
