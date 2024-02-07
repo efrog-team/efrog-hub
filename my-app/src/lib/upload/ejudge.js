88be2f923a6d079f393172633977dd895085dc3e
RagnarLodbrock912 <137827970+RagnarLodbrock912@users.noreply.github.com>
2024-02-07 17:47:51 +0200

    safe boot ejudge

---
+++ my-app/src/lib/upload/ejudge.js
@@ -0,0 +1,157 @@
+const fs = require('fs');
+const fse = require('fs-extra');
+const AdmZip = require('adm-zip');
+const xml2js = require('xml2js');
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// Чтение данных из stdin
+rl.on('line', async (input) => {
+    try {
+        // Ваш код обработки данных
+        const data = JSON.parse(input);
+        const res = await processFile(data.base64String, data.currentSession, data.currentDirectory, data.fileName, data.taskName);
+
+        if (typeof res === 'string') {
+            console.log(res);
+        } else {
+            console.error(res.error);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    } finally {
+        rl.close();
+    }
+});
+
+function processObject(obj) {
+    let result = '';
+  
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        const value = obj[key];
+  
+        result += value;
+
+        if (key === 'p' || key === 'br') {
+          result += '\n';
+        }
+      }
+    }
+    if (result.endsWith('\n')) {
+        result = result.slice(0, -1);
+      }
+
+    return result;
+  }
+
+async function processFile(base64String, curent_sesion, main_dir, fileName, taskName) {
+    try{
+        let name, time_limit, memory_limit, statement, input_statement, output_statement, note;
+        let test = [];
+
+        if (fs.existsSync(`./files/upload/ejudge_${curent_sesion}`)) {
+            fse.removeSync(`./files/upload/ejudge_${curent_sesion}`);
+        }
+        fs.mkdirSync(`./files/upload/ejudge_${curent_sesion}`);
+
+        let targetFilePath = `${main_dir}/files/upload/ejudge_${curent_sesion}/${fileName}`;
+
+        const fileBuffer = Buffer.from(base64String, 'base64');
+        fs.writeFileSync(targetFilePath, fileBuffer);
+        
+        let zip = new AdmZip(targetFilePath);
+        zip.extractAllTo(`${main_dir}/files/upload/ejudge_${curent_sesion}/`);
+        fs.unlinkSync(targetFilePath);
+        
+        let main_path = `${main_dir}/files/upload/ejudge_${curent_sesion}/`;
+        const contests =  fs.readdirSync(main_path);
+        main_path = `${main_path}${contests[0]}/`;
+
+
+        //Check folder with task about nesessary folders and files
+        const directories = ['statements', 'tests', `statements/${taskName}.xml`];
+
+        for(let i = 0; i < directories.length; i++) {
+            if (!fs.existsSync(main_path + directories[i])) {
+                return(JSON.stringify({status: 404, error: `There are not ${directories[i]}`}));
+            }
+        }
+    
+        // Process general info
+        const readFilePromise = new Promise((resolve, reject) => {
+            fs.readFile(`${main_path}statements/${taskName}.xml`, "utf-8", (err, data) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+        const data = await readFilePromise;
+
+        const result = await new Promise((resolve, reject) => {
+            xml2js.parseString(data, (err, result) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+
+        name = result.problem.statement[0].title[0];
+        time_limit = 5;
+        memory_limit = 256;
+
+    
+
+        // Process statement
+        statement = processObject(result.problem.statement[0].description[0]);
+        input_statement = processObject(result.problem.statement[0].input_format[0]);
+        output_statement = processObject(result.problem.statement[0].output_format[0]);
+        note = processObject(result.problem.statement[0].notes[0]);
+
+        // Process test cases
+        test = [];
+        const examples = result.problem.examples[0].example;
+
+        for (let i = 0; i < examples.length; i++) {
+            test.push({
+                test_id: test.length + 1,
+                input: examples[i].input[0],
+                output: examples[i].output[0],
+                status: "Opened"
+            });
+        }
+
+        const userTests = fs.readdirSync(`${main_path}tests/${taskName}`);
+
+        for (let i = 0; i < userTests.length; i++) {
+            if (userTests[i].split(".")[1] == "answ") {
+                const output_format = fs.readFileSync(`${main_path}tests/${taskName}/${userTests[i]}`, 'utf-8');
+
+                const input_format_file = `${userTests[i].split(".")[0]}.dat`;
+                const input_format_path = `${main_path}tests/${taskName}/${input_format_file}`;
+
+                if (!fs.existsSync(input_format_path)) {
+                    return(JSON.stringify({status: 404, error: `There is ${userTests[i]}, but no ${input_format_file}`}));
+                }
+
+                const input_format = fs.readFileSync(input_format_path, 'utf-8');
+                test.push({ test_id: test.length + 1, input: input_format, output: output_format, status: "Closed" });
+            }
+        }
+        return(JSON.stringify({name, time_limit, memory_limit, statement, input_statement, output_statement, note, test}));
+    }
+    catch(e){
+        console.error(e)
+        return({error: e})
+    }
+    
+}
--- my-app/src/routes/api/task/finish/upload-file-ejudge/+server.js
+++ my-app/src/routes/api/task/finish/upload-file-ejudge/+server.js
@@ -1,34 +1,10 @@
 import { json } from '@sveltejs/kit';
-import AdmZip from 'adm-zip';
-import xml2js from 'xml2js';
-import fs from 'fs';
 import fse from 'fs-extra';
 import { getClientrId } from '$lib/server/main';
-import { authorization, checkValue } from '$lib/server/check.js';
+import { authorization } from '$lib/server/check.js';
+import { spawnSync } from 'child_process';
+import path from 'path';
 
-let name, time_limit, memory_limit, statement, input_statement, output_statement, note;
-let test = [];
-
-function processObject(obj) {
-    let result = '';
-  
-    for (const key in obj) {
-      if (obj.hasOwnProperty(key)) {
-        const value = obj[key];
-  
-        result += value;
-
-        if (key === 'p' || key === 'br') {
-          result += '\n';
-        }
-      }
-    }
-    if (result.endsWith('\n')) {
-        result = result.slice(0, -1);
-      }
-
-    return result;
-  }
 
 /** @type {import('./$types').RequestHandler} */
 export async function POST( {request, cookies} ) {
@@ -47,106 +23,67 @@ export async function POST( {request, cookies} ) {
 
         // Get task arhive and save them into folder
         const file = formData.get('file');
-
-        if (fs.existsSync(`./files/upload/ejudge_${curent_sesion}`)) {
-            fse.removeSync(`./files/upload/ejudge_${curent_sesion}`);
-        }
-        fs.mkdirSync(`./files/upload/ejudge_${curent_sesion}`);
-
-        let targetFilePath = `./files/upload/ejudge_${curent_sesion}/${file.name}`;
-        const fileBuffer = await file.arrayBuffer();
-        fs.writeFileSync(targetFilePath, new Uint8Array(fileBuffer));
-
-        let zip = new AdmZip(targetFilePath);
-        zip.extractAllTo("./files/upload/ejudge_" + curent_sesion + "/");
-        fs.unlinkSync(targetFilePath);
-
-        let main_path = `./files/upload/ejudge_${curent_sesion}/`;
-        let error_path = "./static/upload/ejudge_" + curent_sesion;
-        const contests =  fs.readdirSync(main_path);
-        main_path = `${main_path}${contests[0]}/`;
-
-
-        //Check folder with task about nesessary folders and files
         const taskName = formData.get('taskName');
-        const directories = ['statements', 'tests', `statements/${taskName}.xml`];
+        const fileName = file.name;
 
-        for(let i = 0; i < directories.length; i++) {
-            if (!fs.existsSync(main_path + directories[i])) {
-                fse.removeSync(error_path);
-                return json({ error: `There are not ${directories[i]}` }, { status: 404 });  
-            }
+        const reader = file.stream().getReader();
+        let chunks = [];
+
+        while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+
+        chunks.push(value);
         }
-    
-        // Process general info
-        const readFilePromise = new Promise((resolve, reject) => {
-            fs.readFile(`${main_path}statements/${taskName}.xml`, "utf-8", (err, data) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(data);
-                }
-            });
+
+        const fileBuffer = Buffer.concat(chunks);
+        const base64String = fileBuffer.toString('base64');
+
+        const command = 'node';
+        // Get patht to process file
+        const targetFileName = 'src/lib/upload/ejudge.js';
+        const currentDirectory = path.resolve();
+        const scriptPath = path.resolve(currentDirectory, targetFileName);
+
+        const inputData = JSON.stringify({
+            base64String,
+            curent_sesion,
+            currentDirectory,
+            fileName,
+            taskName
         });
-        const data = await readFilePromise;
+        
+        const options = {
+            input: inputData, 
+            stdio: 'pipe',
+            uid: 65534
+        };
+        
+        const result = spawnSync(command, [scriptPath], options);
 
-        const result = await new Promise((resolve, reject) => {
-            xml2js.parseString(data, (err, result) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(result);
+        if (result.status === 0) {
+            if (result.stdout) {
+                const data = JSON.parse(result.stdout.toString());
+                if(data.error){
+                    return json({ error: data.error }, { status: data.status });
                 }
-            });
-        });
-    
-        name = result.problem.statement[0].title[0];
-        time_limit = 5;
-        memory_limit = 256;
-
-    
-
-        // Process statement
-        statement = processObject(result.problem.statement[0].description[0]);
-        input_statement = processObject(result.problem.statement[0].input_format[0]);
-        output_statement = processObject(result.problem.statement[0].output_format[0]);
-        note = processObject(result.problem.statement[0].notes[0]);
-
-        // Process test cases
-        test = [];
-        const examples = result.problem.examples[0].example;
-
-        for (let i = 0; i < examples.length; i++) {
-            test.push({
-                test_id: test.length + 1,
-                input: examples[i].input[0],
-                output: examples[i].output[0],
-                status: "Opened"
-            });
-        }
-
-        const userTests = fs.readdirSync(`${main_path}tests/${taskName}`);
-
-        for (let i = 0; i < userTests.length; i++) {
-            if (userTests[i].split(".")[1] == "answ") {
-                const output_format = fs.readFileSync(`${main_path}tests/${taskName}/${userTests[i]}`, 'utf-8');
-
-                const input_format_file = `${userTests[i].split(".")[0]}.dat`;
-                const input_format_path = `${main_path}tests/${taskName}/${input_format_file}`;
-
-                if (!fs.existsSync(input_format_path)) {
-                    fse.removeSync(error_path);
-                    return json({ error: `There is ${userTests[i]}, but no ${input_format_file}` }, { status: 404 });
+                else{
+                    return(json(data));
                 }
-
-                const input_format = fs.readFileSync(input_format_path, 'utf-8');
-                test.push({ test_id: test.length + 1, input: input_format, output: output_format, status: "Closed" });
+            } else {
+                return json({ error: "Unexpected error occurred during reading data" }, { status: 500 });
             }
-        }
 
-        // Return task data
-        return json({name, time_limit, memory_limit, statement, input_statement, output_statement, note, test});
-
+        } else{
+            if (result.stderr) {
+                const stderrString = result.stderr.toString();
+                console.log(stderrString);
+                return json({ error: stderrString }, { status: 404 });
+            } else {
+                return json({ error: "Unexpected error occurred" }, { status: 500 });
+            }
+            
+        };
     } catch (error) {
         // In instance of error return error
         console.log(error);
